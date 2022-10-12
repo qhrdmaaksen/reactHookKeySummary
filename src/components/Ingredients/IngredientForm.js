5afbb8a760cd70bff4b51b9a1c106bef7c29aa80
vitamin777 <rlaalsdn8@gmail.com>
2022-10-12 23:13:20 +0900

    feat : 입력 값 받아 데이터화
    -IngredientForm.js 에서 useState 로 입력에 대한 value 가 바뀔때마다
    Handler 함수에서 setState 로 입력 받아 submitHandler 에서 title,amount 에 대해
    입력 값을 설정하여 ingredientData 넣어줌

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,28 +1,55 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+  const [enteredIngredient, setEnteredIngredient] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+
+  /*const inputIngredientRef = useRef();  // useRef()를 사용하면 ref 를 만들 수 있다. ref 는 DOM 에 접근할 수 있게 해준다.
+  const inputAmountRef = useRef();*/
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
+    /*const enteredIngredient = inputIngredientRef.current.value; // ref 를 사용하면 DOM 에 접근할 수 있게 해준다.
+    const enteredAmount = inputAmountRef.current.value;*/
+
+    const ingredientData = {
+      title: enteredIngredient,
+      amount: enteredAmount,
+    }
+    console.log(ingredientData);
+
+    /*props.onAddIngredient(ingredientData);*/
+
   };
+
+  const ingredientChangeHandler = (event) => {
+    setEnteredIngredient(event.target.value)
+  }
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value)
+  }
 
   return (
     <section className="ingredient-form">
       <Card>
         <form onSubmit={submitHandler}>
           <div className="form-control">
-            <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <label htmlFor="title">재료명</label>
+            {/*<input type="text" id="title" ref={inputIngredientRef}/>*/}
+            <input type="text" id="title" value={enteredIngredient} onChange={ingredientChangeHandler}/>
           </div>
           <div className="form-control">
-            <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <label htmlFor="amount">수량</label>
+            {/*<input type="number" id="amount" ref={inputAmountRef}/>*/}
+            <input type="number" id="amount" value={enteredAmount} onChange={amountChangeHandler}/>
           </div>
           <div className="ingredient-form__actions">
-            <button type="submit">Add Ingredient</button>
+            <button type="submit">재료 추가</button>
           </div>
         </form>
       </Card>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//react 18 version
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
+/*react 17 version
+ReactDOM.render(<App />, document.getElementById('root'));*/
