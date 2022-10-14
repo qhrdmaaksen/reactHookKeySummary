0ebb94df4bebec409e26e6128aa373c1ae9af910
vitamin777 <rlaalsdn8@gmail.com>
2022-10-14 23:41:24 +0900

    feat : 추가된 재료 목록 불러오기 추가
    -새로고침 이후에도 재료 목록 데이터가 출력 될 수 있도록 fetch 로 파이어베이스 url
    에 get 요청으로 접근하여 응답 받은 객체 데이터를 배열에 담아 넣어 setUserIngredients state 로 업데이트 해주도록하고 useEffect 로
    빈 배열 설정 해줘서 렌더링 후 한번만 겟 요청 하도록 설정함
    
    -기능 정상 작동 확인

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -8,6 +8,30 @@ function Ingredients() {
   /*ingredients 에 재료 목록이 저장되때문에 배열로 초기값 설정
    * 목록은 항상 전체가 업데이트되기 때문, 재료가 추가되거나 삭제될 경우*/
   const [userIngredients, setUserIngredients] = useState([]);
+
+  useEffect(() => {
+    /*useEffect 미 사용 시 무한 루프 발생
+데이터를 렌더링 함수 안에서 가져오게 되면, 재료 목록이 렌더링 될 때마다 HTTP 요청을 전송하게
+됩니다, 그리고 HTTP 요청을 보낸 뒤에는 상태(state)를 업데이트하죠, 상태를
+업데이트하면 무슨 일이 일어나죠? 컴포넌트가 리렌더링 됩니다, 그럼 재료 목록도 리렌더링 되기때문
+useEffect 에 의존성배열인 빈 배열을 넣게되면 useEffect 안에 로직은 첫 전체 렌더링 후 한번만 실행되게됨*/
+    /*firebase 에 저장된 재료 데이터 GET 요청*/
+    fetch(
+        'https://react-hooks-update-4630f-default-rtdb.firebaseio.com/ingredients.json',
+    )
+        .then((response) => response.json())
+        .then((responseData) => {
+          const loadedIngredients = [];
+          for (const key in responseData) {
+            loadedIngredients.push({
+              id: key,
+              title: responseData[key].title,
+              amount: responseData[key].amount,
+            });
+          }
+          setUserIngredients(loadedIngredients);
+        });
+  }, []);
 
   /*이 함수는 새로 추가할 ingredient 를 받으며 받은 값은 배열에 저장되어야함*/
   const addIngredientHandler = (ingredient) => {
@@ -29,19 +53,20 @@ function Ingredients() {
         body: JSON.stringify(ingredient),
         headers: { 'Content-Type': 'application/json' },
       },
-    ).then((response) => {
-      /*response.json() 을 통해 반환된 데이터는 then() 의 콜백 함수의 인자로 전달됨
-        * 이 데이터는 Firebase 에서 생성된 고유한 ID 값임
-        * 이 ID 값을 ingredient 객체에 추가하고, userIngredients 에 추가*/
-      return response.json();
-    }).then((responseData) => {
-      setUserIngredients((prevIngredients) => [
-        ...prevIngredients,
-        /*...ingredient 는 기존의 배열에 새로운 요소를 추가하고 새로운 배열을 반환한다.
-         * 새로운 요소는 {id: 'id', title: 'title', amount: 'amount'} 형태로 추가된다.*/
-        { id: responseData.name, ...ingredient },
-      ]);
-    })
+    )
+      .then((response) => {
+        /*response.json() 을 통해 반환된 데이터는 then() 의 콜백 함수의 인자로 전달됨
+         * 이 데이터는 Firebase 에서 생성된 고유한 ID 값임*/
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          /*...ingredient 는 기존의 배열에 새로운 요소를 추가하고 새로운 배열을 반환한다.
+           * 새로운 요소는 {id: 'id', title: 'title', amount: 'amount'} 형태로 추가된다.*/
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
   };
 
   return (
