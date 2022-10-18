fdd141c1ffeb3fe164773181fdf116af2899352f
vitamin777 <rlaalsdn8@gmail.com>
2022-10-18 23:09:19 +0900

    refactor : loading, error useState 를 useReducer 로 통합 관리
    -컴포넌트 바깥에 httpReducer 상태 관리 로직 작성
    ,loading 표시기가 필요한 추가 및 삭제에 dispatchHttp type 으로 액션을 리듀서에 보내서 액션에 설정된것으로 loading 관리
    ,error 또한 마찬가지로 .catch() 에 dispatchHttp type error 로 요청 구간에 발생하는 에러설정
    ,jsx 에서 error 를 httpState.error 로 접근해 에러 발생시 모달창 팝업되도록함
    
    -기능 정상 작동 확인

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -14,9 +14,28 @@ const ingredientReducer = (currentIngredients, action) => {
     case 'ADD': //action.type 이 ADD 이면 action.ingredient 를 currentIngredients 에 추가 한다.
       return [...currentIngredients, action.ingredient];
     case 'DELETE': //action.type 이 DELETE 이면 action.id 를 currentIngredients 에서 제거 한다.
-      return currentIngredients.filter(ing => ing.id !== action.id)
+      return currentIngredients.filter((ing) => ing.id !== action.id);
     default:
-      throw new Error('Should not get there!');
+      throw new Error('ingredientReducer::Should not get there!');
+  }
+};
+// httpReducer 는 loading 과 error 를 관리한다.
+// loading 은 true/false 로 관리하고, error 는 null/string 으로 관리한다.
+// loading 은 http 요청을 보내는 중인지 아닌지를 나타내고, error 는 http 요청이 실패했을 때 에러 메시지를 나타낸다.
+// http 요청을 보내는 중이면 loading 을 true 로, 요청이 완료되면 loading 을 false 로 설정한다.
+// http 요청이 실패하면 error 에 에러 메시지를 설정하고, http 요청이 성공하면 error 를 null 로 설정한다.
+const httpReducer = (curHttpState, action) => {
+  switch (action.type) {
+    case 'SEND':
+      return { loading: true, error: null };
+    case 'RESPONSE':
+      return { ...curHttpState, loading: false };
+    case 'ERROR':
+      return { loading: false, error: action.errorMessage };
+    case 'CLEAR':
+      return { ...curHttpState, error: null };
+    default:
+      throw new Error('httpReducer::Should not be reached!');
   }
 };
 
@@ -26,10 +45,17 @@ function Ingredients() {
   dispatch 는 action 객체를 인자로 전달 받는다. action 객체는 type 프로퍼티를 가지고 있어야 한다.
   action 객체의 type 프로퍼티에 따라 reducer 함수가 호출된다. reducer 함수는 action 객체를 인자로 전달 받는다.
   reducer 함수는 action 객체의 type 프로퍼티에 따라 다른 로직을 수행한다.*/
-  const [userIngredients, dispatch]=useReducer(ingredientReducer, []);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  //useReducer 를 사용하여 httpState 를 관리한다.
+  //httpReducer 는 reducer 함수이고, {loading: false, error: null} 는 초기값이다.
+  const [httpState, dispatchHttp] = useReducer(httpReducer, {
+    loading: false,
+    error: null,
+  });
+
+  //  const [isLoading, setIsLoading] = useState(false);
+  //const [error, setError] = useState();
 
   /*ingredients 에 재료 목록이 저장되때문에 배열로 초기값 설정
    * 목록은 항상 전체가 업데이트되기 때문, 재료가 추가되거나 삭제될 경우*/
@@ -43,12 +69,13 @@ function Ingredients() {
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
     //setUserIngredients(filteredIngredients);
     //dispatch 함수를 호출하여 reducer 함수를 호출한다.
-    dispatch({type: 'SET', ingredients: filteredIngredients})
+    dispatch({ type: 'SET', ingredients: filteredIngredients });
   }, []);
 
   /*이 함수는 새로 추가할 ingredient 를 받으며 받은 값은 배열에 저장되어야함*/
   const addIngredientHandler = (ingredient) => {
-    setIsLoading(true);
+    //setIsLoading(true);
+    dispatchHttp({type: 'SEND'});
     /*fetch() 는 브라우저에서 제공하는 API 로서,  HTTP 요청을 보내는 함수
      * 첫번째 인자로 요청할 URL 을 전달하고, 두번째 인자로 요청에 대한 설정을 전달
      * 설정에는 method, body, header 등이 있음
@@ -69,7 +96,8 @@ function Ingredients() {
       },
     )
       .then((response) => {
-        setIsLoading(false);
+        //setIsLoading(false);
+        dispatchHttp({type: 'RESPONSE'});
         /*response.json() 을 통해 반환된 데이터는 then() 의 콜백 함수의 인자로 전달됨
          * 이 데이터는 Firebase 에서 생성된 고유한 ID 값임*/
         return response.json();
@@ -77,8 +105,8 @@ function Ingredients() {
       .then((responseData) => {
         dispatch({
           type: 'ADD',
-          ingredient: {id: responseData.name, ...ingredient}
-        })
+          ingredient: { id: responseData.name, ...ingredient },
+        });
         /*setUserIngredients((prevIngredients) => [
           ...prevIngredients,
           /!*...ingredient 는 기존의 배열에 새로운 요소를 추가하고 새로운 배열을 반환한다.
@@ -87,14 +115,16 @@ function Ingredients() {
         ]);*/
       })
       .catch((error) => {
-        setError('재료 추가 요청 처리 중 에러가 발생했습니다.');
-        setIsLoading(false);
+        /*setError('재료 추가 요청 처리 중 에러가 발생했습니다.');
+        setIsLoading(false);*/
+        dispatchHttp({type: 'ERROR', errorMessage: '재료 추가 요청 처리 중 에러가 발생했습니다.'});
       });
   };
 
   /*이 함수는 삭제할 ingredient 의 id 를 받으며 받은 값은 배열에서 제거되어야함*/
   const removeIngredientHandler = (ingredientId) => {
-    setIsLoading(true);
+    //setIsLoading(true);
+    dispatchHttp({type: 'SEND'});
     // Firebase 에서 데이터를 삭제할 때는 해당 데이터의 고유한 ID 값을 사용
     fetch(
       `https://react-hooks-update-4630f-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,
@@ -103,13 +133,14 @@ function Ingredients() {
       },
     )
       .then((response) => {
-        setIsLoading(false);
+        //setIsLoading(false);
+        dispatchHttp({type: 'RESPONSE'});
         // 삭제가 완료되면, 기존의 배열에서 해당 요소를 제거
 
         dispatch({
           type: 'DELETE',
           id: ingredientId,
-        })
+        });
 
         /*setUserIngredients((prevIngredients) =>
           // filter() 는 배열의 요소를 순회하며, 콜백 함수의 조건에 맞는 요소만 반환
@@ -125,9 +156,9 @@ function Ingredients() {
       })
       .catch((error) => {
         // 에러가 발생하면, 에러 메시지를 출력하고, 로딩 상태를 false 로 변경
-
-        setError('재료 삭제 요청 처리 중 에러가 발생하였습니다.');
-        setIsLoading(false);
+        /*setError('재료 삭제 요청 처리 중 에러가 발생하였습니다.');
+        setIsLoading(false);*/
+        dispatchHttp({type: 'ERROR', errorMessage: '재료 삭제 요청 처리 중 에러가 발생하였습니다.'});
       });
   };
 
@@ -135,15 +166,16 @@ function Ingredients() {
     // 이후, 에러 메시지를 초기화
     // 이 함수는 IngredientForm 컴포넌트에서 사용됨
     // 에러 메시지가 발생하면, 에러 메시지를 출력하고, 이후에는 에러 메시지를 초기화
-    setError(null);
+    //setError(null);
+    dispatchHttp({type: 'CLEAR'});
   };
 
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
       <IngredientForm
         onAddIngredient={addIngredientHandler}
-        loading={isLoading}
+        loading={httpState.loading}
       />
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
